d280acccae900605fea9e8e6519a098aec2bbf2e
stefz <65717831+st3fz@users.noreply.github.com>
2020-09-02 04:28:20 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,116 +1,128 @@
 function diceRoll() {
-  // 1st dice
-  var randomNumber1 = Math.floor(Math.random() * 6 + 1);
-  var Image1 = "dice" + randomNumber1 + ".png";
+
+  // Dice animation
+  $(".dice").addClass("rotate");
+  $(".dice").on("animationend", function() {
+    $(".dice").removeClass("rotate");
+  })
+
+  // Diceroll
+  randomNumber1 = Math.floor(Math.random() * 6 + 1);
+  Image1 = "dice" + randomNumber1 + ".png";
   document.querySelectorAll("img")[1].setAttribute("src", Image1);
 
-  // 2nd dice
-  var randomNumber2 = Math.floor(Math.random() * 6 + 1);
-  var Image2 = "dice" + randomNumber2 + ".png";
+  randomNumber2 = Math.floor(Math.random() * 6 + 1);
+  Image2 = "dice" + randomNumber2 + ".png";
   document.querySelectorAll("img")[2].setAttribute("src", Image2);
 
-  // 3rd dice
-  var randomNumber3 = Math.floor(Math.random() * 6 + 1);
-  var Image3 = "dice" + randomNumber3 + ".png";
+  randomNumber3 = Math.floor(Math.random() * 6 + 1);
+  Image3 = "dice" + randomNumber3 + ".png";
   document.querySelectorAll("img")[3].setAttribute("src", Image3);
 
-  // 4th dice
-  var randomNumber3 = Math.floor(Math.random() * 6 + 1);
-  var Image4 = "dice" + randomNumber3 + ".png";
+  randomNumber4 = Math.floor(Math.random() * 6 + 1);
+  Image4 = "dice" + randomNumber3 + ".png";
   document.querySelectorAll("img")[4].setAttribute("src", Image4);
 }
-
 
 // Responding to Dropdown Button
 let links = document.querySelectorAll('#list li')
 links.forEach((el) => {
   el.addEventListener('click', (event) => {
+
     let numberOfChoices = event.target.innerText
     document.getElementById('dropdownMenu').innerHTML = `${numberOfChoices}<span class="caret"></span>`
 
     if (numberOfChoices === "2") {
-      $("#img3, #img4, .threeChoices, .fourChoices").addClass("invisible")
+      $(".3Choices, .4Choices").addClass("invisible")
     }
     if (numberOfChoices === "3") {
-      $("#img4, .fourChoices").addClass("invisible");
-      $("#img3, .threeChoices").removeClass("invisible")
+      $(".4Choices").addClass("invisible");
+      $(".3Choices").removeClass("invisible")
     }
     if (numberOfChoices === "4") {
-      $("#img3, #img4, .threeChoices, .fourChoices").removeClass("invisible");
+      $(".3Choices, .4Choices").removeClass("invisible");
     }
   })
 })
 
+// Variables
+var randomNumber1;
+var randomNumber2;
+var randomNumber3;
+var randomNumber4;
+var Image1;
+var Image2;
+var Image3;
+var Image4;
+var isTie;
+var winNo;
+var userChoices = [];
+var chosen;
+var winnerPosition;
+function printWin() {$("#title").html(chosen + " wins! 🏆");}
+
 // Responding to Submit
-document.getElementById("submit").addEventListener("click", function(e) {
+$("#submit").click(function(e) {
   e.preventDefault();
 
-// Storing Data into variables
+  $("#submit").html("Again");
+  diceRoll();
+  // Storing Data into variables
   var choice1 = $("#choice1").val();
   var choice2 = $("#choice2").val();
   var choice3 = $("#choice3").val();
   var choice4 = $("#choice4").val();
   var noOfChoices = $("#dropdownMenu").text();
+  var diceValues = [];
 
-// Rotate animation
-  $(".dice").addClass("rotate");
+  // Display user input
+  $("#caption1").html(choice1);
+  $("#caption2").html(choice2);
+  $("#caption3").html(choice3);
+  $("#caption4").html(choice4);
 
-// Changing text to user input
-      $("#caption1").html(choice1);
-      $("#caption2").html(choice2);
-      $("#caption3").html(choice3);
-      $("#caption4").html(choice4);
-console.log(noOfChoices);
-
-// Displaying no. of dices that user chose
-    if (noOfChoices === "2") {
-      $("#caption1, #caption2").removeClass("invisible");
-      $("#caption3, #caption4").addClass("invisible");
+  // Winner
+  if (noOfChoices === "2") {
+    if (randomNumber1 > randomNumber2) {
+      $("#title").html(choice1 + " wins! 🏆");
+    } else if (randomNumber2 > randomNumber1) {
+      $("#title").html(choice2 + " wins! 🏆");
+    } else if (randomNumber2 = randomNumber1) {
+      $("#title").html("Oops, try again!");
     }
+  }
 
-    if (noOfChoices === "3") {
-      $("#caption1, #caption2, #caption3").removeClass("invisible");
-      $("#caption4").addClass("invisible");
+  if (noOfChoices === "3") {
+    userChoices = [choice1, choice2, choice3];
+
+    diceValues.push(randomNumber1, randomNumber2, randomNumber3);
+    let maxDice = Math.max(...diceValues);
+    diceValues.filter(x => x === maxDice).length > 1 ? isTie = "True" : isTie = "False";
+    if (isTie === "True") {
+      $("#title").html("Oops, try again!");
+
+    } else if (isTie === "False") {
+      winnerPosition = diceValues.indexOf(maxDice);
+      chosen = userChoices[winnerPosition];
+    printWin();
+    console.log("winner: " + winnerPosition + " and chosen: " + chosen);
     }
+  }
 
-    if (noOfChoices === "4") {
-      $(".caption").removeClass("invisible");
+  if (noOfChoices === "4") {
+    userChoices = [choice1, choice2, choice3, choice4];
+
+    diceValues.push(randomNumber1, randomNumber2, randomNumber3, randomNumber4);
+    let maxDice = Math.max(...diceValues);
+    diceValues.filter(x => x === maxDice).length > 1 ? isTie = "True" : isTie = "False";
+    if (isTie === "True") {
+      $("#title").html("Oops, try again!");
+
+    } else if (isTie === "False") {
+      winnerPosition = diceValues.indexOf(maxDice);
+      chosen = userChoices[winnerPosition];
+    printWin();
+    console.log("winner: " + winnerPosition + " and chosen: " + chosen);
     }
-
-$("#submit").html("Again");
-
-// Rolling Dice
-// 1st dice
-var randomNumber1 = Math.floor(Math.random() * 6 + 1);
-var Image1 = "dice" + randomNumber1 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", Image1);
-
-// 2nd dice
-var randomNumber2 = Math.floor(Math.random() * 6 + 1);
-var Image2 = "dice" + randomNumber2 + ".png";
-document.querySelectorAll("img")[2].setAttribute("src", Image2);
-
-// 3rd dice
-var randomNumber3 = Math.floor(Math.random() * 6 + 1);
-var Image3 = "dice" + randomNumber3 + ".png";
-document.querySelectorAll("img")[3].setAttribute("src", Image3);
-
-// 4th dice
-var randomNumber3 = Math.floor(Math.random() * 6 + 1);
-var Image4 = "dice" + randomNumber3 + ".png";
-document.querySelectorAll("img")[4].setAttribute("src", Image4);
-
-// Determining winner
-if (noOfChoices === "2") {if (randomNumber1 > randomNumber2) {$("#title").html(choice1 + "wins! 🏆");}
-
-else if (randomNumber2 > randomNumber1) {$("#title").html(choice2 + "wins! 🏆");}
-
-else if (randomNumber2 = randomNumber1){$("#title").html("Oops, try again!");}
-
-
-}
-
-})
-
-
-var diceNumbers = []
+  }
+});
